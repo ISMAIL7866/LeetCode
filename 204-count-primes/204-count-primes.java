3f631d9201a087248e319b127c8c68616ba4c481
ISMAIL7866 <92500302+ISMAIL7866@users.noreply.github.com>
2022-09-04 10:51:38 +0530

    Time: 259 ms (21.21%), Space: 67.9 MB (82.43%) - LeetHub

---
+++ 204-count-primes/204-count-primes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countPrimes(int n) {
+        if(n<2) return 0;
+        boolean [] prime = new boolean[n];
+        Arrays.fill(prime, true);
+        prime[0] = prime [1] = false;
+        
+        int count=0;
+        for(int i=2 ; i <n; i++){
+            if(prime[i]== false) continue;
+            count++; // i os a prime number
+            for(long j=1l*i;j <n; j = j+i){
+                prime[(int)j]= false; // mark multiples of i as no-prime
+                
+            }
+        }
+        return count;
+    }
+}
